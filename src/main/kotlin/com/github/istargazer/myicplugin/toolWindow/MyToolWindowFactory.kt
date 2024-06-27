2a3c30b84f90469286078b3b5f1d3b1cddf8be97
GitHub Action <action@github.com>
2024-06-27 01:58:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/istargazer/myicplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/istargazer/myicplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.istargazer.myicplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.istargazer.myicplugin.MyBundle
+import com.github.istargazer.myicplugin.services.MyProjectService
 import javax.swing.JButton
 
 
